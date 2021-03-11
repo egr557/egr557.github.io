956602fe3206071823cb5a58d6bf96335f363fff
Dan Aukes <danaukes@gmail.com>
2021-03-11 08:22:26 -0700

    autobuild

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git commit -m"autobuild"
 git push
 bundle exec jekyll build
 rm -rf ~/websites/egr557.github.io/*
-cp -r ~/projects/project_foldable_robotics/_site/* ~/websites/egr557.github.io/
+cp ~/projects/project_foldable_robotics/_site/* ~/websites/egr557.github.io/
 cd ~/websites/egr557.github.io/
 git add *
 git pull
